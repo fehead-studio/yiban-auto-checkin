42ffb18cac9d4bd1cfbc215361ab2a7477cc52ab
jinxuyang <jinxuyang3@gmail.com>
2020-12-06 13:12:28 +0800

    feat<User>: add remove invalid function

---
+++ src/test/java/ink/verge/yiban_auto_checkin/RemoveInvalidUser.java
@@ -0,0 +1,44 @@
+package ink.verge.yiban_auto_checkin;
+
+import cn.hutool.crypto.symmetric.SymmetricCrypto;
+import ink.verge.yiban_auto_checkin.mbg.mapper.UserMapper;
+import ink.verge.yiban_auto_checkin.mbg.model.User;
+import ink.verge.yiban_auto_checkin.mbg.model.UserExample;
+import ink.verge.yiban_auto_checkin.utils.YibanUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ * @Author Verge
+ * @Date 2020/12/6 12:57
+ * @Version 1.0
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+public class RemoveInvalidUser {
+    @Autowired
+    private YibanUtils yibanUtils;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private SymmetricCrypto aes;
+    @Test
+    public void remove(){
+        List<User> userList = userMapper.selectByExample(new UserExample());
+        for (User user : userList) {
+            String password = aes.decryptStr(user.getPassword());
+            if (!yibanUtils.verifyAccount(user.getAccount(),password)){
+                userMapper.deleteByPrimaryKey(user.getUid());
+                log.info("已删除: "+user.getAccount());
+            }
+        }
+    }
+
+}
