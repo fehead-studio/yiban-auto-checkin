b4d6f62e18287222dbe1c54e165f66ab07fc3c5a
lmwis <detectiveygn@163.com>
2020-12-20 12:38:51 +0800

    feat<sms>:enable to send sms

--- src/main/java/ink/verge/yiban_auto_checkin/YibanAutoCheckinApplication.java
+++ src/main/java/ink/verge/yiban_auto_checkin/YibanAutoCheckinApplication.java
@@ -3,10 +3,12 @@ package ink.verge.yiban_auto_checkin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan("com.fehead.lang.**")
 @MapperScan("ink.verge.yiban_auto_checkin.mbg.mapper")
 public class YibanAutoCheckinApplication {
 
