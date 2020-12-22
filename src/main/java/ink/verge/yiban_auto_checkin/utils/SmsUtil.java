67100937a66caab8aa4f3237469e7022dd5dc52c
Nightnessss <nightnessss@163.com>
2020-12-22 23:09:58 +0800

    feat<style>: 优化代码结构

--- src/main/java/ink/verge/yiban_auto_checkin/utils/SmsUtil.java
+++ src/main/java/ink/verge/yiban_auto_checkin/utils/SmsUtil.java
@@ -6,14 +6,12 @@ import com.aliyuncs.CommonResponse;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.fehead.lang.properties.FeheadProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 /**
@@ -40,18 +38,19 @@ import java.util.Map;
 @Component
 public class SmsUtil {
 
-    @Autowired
+    @Resource
     private FeheadProperties feheadProperties;
 
 
     /**
      * 新版本：阿里云云通信发送短信通知
-     * @param modelName 短信模板名，传入的模板必须是在阿里大于“管理中心-短信模板管理”中的可用模板。示例：sms.TemplateNotice
-     * @param modelParam   模板内容里面的变量
-     * @param phone   用户手机号码
+     *
+     * @param modelName  短信模板名，传入的模板必须是在阿里大于“管理中心-短信模板管理”中的可用模板。示例：sms.TemplateNotice
+     * @param modelParam 模板内容里面的变量
+     * @param phone      用户手机号码
      * @return boolean true成功false失败
      */
-    public  boolean sendSms(String modelName, Map<String, String> modelParam, String phone) {
+    public boolean sendSms(String modelName, Map<String, String> modelParam, String phone) {
 
         boolean result = false;
 
@@ -75,13 +74,11 @@ public class SmsUtil {
         request.putQueryParameter("SignName", signName);
         request.putQueryParameter("TemplateCode", modelName);
         request.putQueryParameter("TemplateParam", JSONUtils.toJSONString(modelParam));
-        System.out.println("modelParam: " + JSONUtils.toJSONString(modelParam)) ;
+        System.out.println("modelParam: " + JSONUtils.toJSONString(modelParam));
         try {
             CommonResponse response = client.getCommonResponse(request);
             System.out.println(response.getData());
             result = true;
-        } catch (ServerException e) {
-            e.printStackTrace();
         } catch (ClientException e) {
             e.printStackTrace();
         }
